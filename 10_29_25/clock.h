f92e09ff34500aa15e9ec5c4fd68f3a1fad2ff10
Angela Venable <avenable11@ivytech.edu>
2025-10-29 21:49:49 -0400

    clock with twelve and twentyfour

--- 10_29_25/clock.h
+++ 10_29_25/clock.h
@@ -1,21 +1,45 @@
 #ifndef CLOCK_H
 #define CLOCK_H
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+enum timeType
+{
+    TWELVE = 12,
+    TWENTYFOUR = 24
+};
+
+enum class partType
+{
+    AM = 4,
+    PM = 8
+};
+const partType parts[2] = {partType::AM, partType::PM};
+const std::string partToStr[2] = {"AM", "PM"};
+const timeType formats[2] = {TWELVE, TWENTYFOUR};
 
 class clockType
 {
 public:
-    void setTime(int, int, int);
+    clockType();
+    clockType(int, int = 0, int = 0, timeType = TWENTYFOUR, partType = partType::PM);
+    void setTime(int, int, int, timeType, partType);
     void getTime(int &, int &, int &) const;
-    void printTime() const;
+    std::string printTime() const;
     void incrementSeconds();
     void incrementMinutes();
     void incrementHours();
     bool equalTime(const clockType &) const;
+    bool validHour(int);
+    bool validMinOrSec(int);
 
 private:
     int hr;
     int min;
     int sec;
+    timeType format;
+    partType part;
 };
 
 #endif
--- 10_29_25/main.cpp
+++ 10_29_25/main.cpp
@@ -1,10 +1,64 @@
 #include <iostream>
+#include <cstdlib>
 #include "clock.h"
+
+clockType createClock();
 
 int main()
 {
+    int list[10] = {1};
+    int list2[10];
+    // list2 = list;
+
     clockType c;
-    c.setTime(5, 43, 22);
-    clockType yourClock;
+    c.setTime(5, 43, 22, TWELVE, partType::PM);
+
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+
+    c.getTime(hour, minute, second);
+    std::cout << hour << ":" << minute << ":" << second << std::endl;
+
+    clockType yourClock(20, 1, 34);
+    yourClock.getTime(hour, minute, second);
+    clockType newClock(12);
+    clockType newClock2(12, 34);
+    newClock = newClock2;
+    for (int i = 0; i < 100; i++)
+    {
+        clockType a = createClock();
+        std::cout << a.printTime() << std::endl;
+    }
+
     return 0;
+}
+
+clockType createClock()
+{
+    static int noClocks = 0;
+    int hour;
+    int minute;
+    int second;
+    int format;
+    format = rand() % 2;
+    partType tod = partType::PM;
+    if (!format)
+    {
+        hour = (rand() % 12) + 1;
+        int time = rand() % 2;
+        tod = parts[time];
+    }
+    else
+    {
+        hour = rand() % 24;
+    }
+    minute = rand() % 60;
+    second = rand() % 60;
+
+    clockType newClock(hour, minute, second, formats[format], tod);
+    noClocks++;
+    std::cout << noClocks << " clocks created in the program." << std::endl;
+
+    return newClock;
 }
