3707756476f5f22e936be8720f7e6629d7344466
Angela Venable <avenable11@ivytech.edu>
2025-12-03 18:56:01 -0500

    getaddrinfo server

--- 12_03_25/server.cpp
+++ 12_03_25/server.cpp
@@ -18,11 +18,27 @@ int main(int argc, char *argv[])
     int sockfd;
     int clientfd;
     addrinfo hints;
+    addrinfo *servInfo;
+    addrinfo *p;
+    int rv; // return value
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;       // IPv4
     hints.ai_socktype = SOCK_STREAM; // TCP socket
     hints.ai_flags = AI_PASSIVE;     // use my IP;
+
+    try
+    {
+        rv = getaddrinfo(NULL, "9431", &hints, &servInfo);
+        if (rv != 0)
+        {
+            throw std::runtime_error("getaddrinfo error");
+        }
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << e.what() << " " << gai_strerror(rv) << '\n';
+    }
 
     return 0;
 }
