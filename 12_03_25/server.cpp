92d094b924c85e0635732e8563e844b99097c2db
Angela Venable <avenable11@ivytech.edu>
2025-12-03 19:27:47 -0500

    server up to listen

--- 12_03_25/server.cpp
+++ 12_03_25/server.cpp
@@ -13,6 +13,8 @@
 #include <errno.h>
 #include <bits/stdc++.h>
 
+const int BACKLOG = 10;
+
 int main(int argc, char *argv[])
 {
     int sockfd;
@@ -20,7 +22,8 @@ int main(int argc, char *argv[])
     addrinfo hints;
     addrinfo *servInfo;
     addrinfo *p;
-    int rv; // return value
+    int rv;      // return value
+    int yes = 1; // reuse port
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;       // IPv4
@@ -34,10 +37,36 @@ int main(int argc, char *argv[])
         {
             throw std::runtime_error("getaddrinfo error");
         }
+        sockfd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
+        if (sockfd == -1)
+        {
+            throw std::invalid_argument("There was an error creating the socket");
+        }
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        rv = bind(sockfd, servInfo->ai_addr, servInfo->ai_addrlen);
+        if (rv == -1)
+        {
+            close(sockfd);
+            throw std::invalid_argument("Server is unable to bind");
+        }
+        rv = listen(sockfd, BACKLOG);
+        if (rv == -1)
+        {
+            close(sockfd);
+            throw std::invalid_argument("Server is unable to listen");
+        }
+        std::cout << "server: waiting for connections..." << std::endl;
+        while (true)
+        {
+        }
     }
     catch (const std::runtime_error &e)
     {
         std::cerr << e.what() << " " << gai_strerror(rv) << '\n';
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
     }
 
     return 0;
