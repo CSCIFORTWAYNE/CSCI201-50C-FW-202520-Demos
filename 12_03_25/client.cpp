0a2dd32d9ff2f8d444ee97b98c85ccb1c066d048
Angela Venable <avenable11@ivytech.edu>
2025-12-03 19:48:32 -0500

    start of client code

---
+++ 12_03_25/client.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <stdexcept>
+#include <limits>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <bits/stdc++.h>
+
+void resetStream();
+
+int main(int argc, char *argv[])
+{
+    addrinfo hints;
+    addrinfo *servInfo;
+    char s[INET_ADDRSTRLEN];
+    int rv; // return value
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./client <server ip address>" << std::endl;
+        return 1;
+    }
+    try
+    {
+        rv = getaddrinfo(argv[1], "9431", &hints, &servInfo);
+        if (rv != 0)
+        {
+            throw std::runtime_error("getaddrinfo error ");
+        }
+        int sock;
+        sock = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
+        if (sock == -1)
+        {
+            throw std::logic_error("Unable to open socket");
+        }
+        inet_ntop(servInfo->ai_family, (sockaddr_in *)servInfo->ai_addr, s, sizeof(s));
+        std::cout << "client: attempting connection to " << s << std::endl;
+        rv = connect(sock, servInfo->ai_addr, servInfo->ai_addrlen);
+        if (rv == -1)
+        {
+            close(sock);
+            throw std::logic_error("Unable to connect to server");
+        }
+        inet_ntop(servInfo->ai_family, (struct sockaddr_in *)servInfo->ai_addr, s, sizeof(s));
+        std::cout << "client: connected to " << s << std::endl;
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << e.what() << " " << gai_strerror(rv) << std::endl;
+    }
+    catch (std::logic_error e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+void resetStream()
+{
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
